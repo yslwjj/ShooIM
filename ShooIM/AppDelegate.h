eb89b0ae5627c3bfa1a334ccfa1abb7b808de192
sam <samyang1107@icloud.com>
2015-08-20 22:09:34 +0800

    Initial Commit

---
+++ ShooIM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShooIM
+//
+//  Created by 杨森 on 15/8/20.
+//  Copyright (c) 2015年 samyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
